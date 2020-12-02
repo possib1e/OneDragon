78b2075f1f726c10dd5b83af87e4dcfae1ff5891
possib1e <possib1e@126.com>
2020-12-02 21:56:30 +0800

    1

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 import os,sys
-from OneDragon.module.scan_subdomain import start_oneforall
-from OneDragon.module.scan_port import start_masscan_to_nmap
-from OneDragon.module.scan_file import start_ffuf
-from OneDragon.module.scan_vuln import start_xray_scan
-from OneDragon.module.common import read_file
+from module.scan_subdomain import start_oneforall
+from module.scan_port import start_masscan_to_nmap
+from module.scan_file import start_ffuf
+from module.scan_vuln import start_xray_scan
+from module.common import read_file
 #python3 start.py targets.txt
 
 if __name__ == '__main__':
